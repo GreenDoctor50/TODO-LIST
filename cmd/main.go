b899ca93ac4d4e1c1266e1036afdd4ff5faaf6f0
Alex <GreenDoctor50@gmail.com>
2024-04-08 21:27:52 +0300

    3

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,24 @@
+// cmd/main.go
 package main
 
 import (
 	"fmt"
 	"net/http"
 
+	"github.com/GreenDoctor50/todo-list/database"
+	"github.com/GreenDoctor50/todo-list/handlers"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	database.ConnectDB()
+
 	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Привет, программист!"))
-	})
+	r.Get("/tasks", handlers.GetTasks)
+	r.Post("/tasks", handlers.PostTask)
+	r.Get("/tasks/{id}", handlers.GetTaskByID)
+	r.Delete("/tasks/{id}", handlers.DeleteTask)
 
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		fmt.Printf("Start server error: %s", err.Error())
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/FreenDoctor50/todo-list
 
 go 1.21.6
 
-require github.com/go-chi/chi/v5 v5.0.12
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	gorm.io/gorm v1.25.9
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
